c2145b950c8198df5571a1657f82f1cbba68df87
W0FV1 <wofbi1@outlook.com>
2025-01-14 08:59:13 +0800

    增加page section

--- src/main/java/dev/w0fv1/vaadmin/view/SectionItem.java
+++ src/main/java/dev/w0fv1/vaadmin/view/SectionItem.java
@@ -1,26 +1,27 @@
 package dev.w0fv1.vaadmin.view;
+
 import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-public class Section extends VerticalLayout {
+public class SectionItem extends VerticalLayout {
     private final VerticalLayout verticalLayout = new VerticalLayout();
 
-    public Section(String title, String description) {
+    public SectionItem(String title, String description) {
         super();
-        add(new H3(title));
+        add(new H4(title));
         if (description != null && !description.isEmpty()) {
             add(new Span(description));
         }
         add(verticalLayout);
         verticalLayout.setPadding(false);
+        setPadding(false);
     }
-
-
-
-    public void addModule(Component component) {
+    public SectionItem(String title) {
+        this(title, "");
+    }
+    public void addSubModule(Component component){
         verticalLayout.add(component);
     }
 }
